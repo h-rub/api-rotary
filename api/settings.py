e81fda2702d32788304d118d0d9cf6d948bb2aea
h-rub <rubio@loopgk.com>
2021-10-25 16:34:31 -0500

    Ready to production - Bug fix

--- api/settings.py
+++ api/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    #'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # Simplified static file serving.
