86d89ad6f06ec7a3261bf2ae900d42cd502f81ed
h-rub <rubio@loopgk.com>
2021-11-02 10:56:29 -0600

    Test Full Name

--- authentication/serializer.py
+++ authentication/serializer.py
@@ -33,18 +33,25 @@ class UserSerializer(serializers.ModelSerializer):
 class MembersSerializer(serializers.ModelSerializer):
 
     full_name = serializers.SerializerMethodField('get_full_name')
-    username = serializers.SerializerMethodField('get_username')
+    user_name = serializers.SerializerMethodField('get_username')
+    picture = serializers.SerializerMethodField('get_profile_picture')
     class Meta:
         model = CustomUser
-        fields = ['pk','full_name','username']
+        fields = ['pk','full_name','user_name','picture']
 
     def get_full_name(self, CustomUser):
         full_name = CustomUser.get_full_name_user()
         return full_name['full_name']
     
     def get_username(self,CustomUser):
-        username = CustomUser.get_username()
-        return username
+        user_name = CustomUser.get_username()
+        return user_name
+
+    def get_profile_picture(self, CustomUser, Profile):
+        _id = CustomUser.pk
+        profile = Profile.objects.get(pk=_id)
+        picture = profile.profile_picture
+        return str(picture)
 
 class UpdateProfileSerializer(serializers.ModelSerializer):
     class Meta:
