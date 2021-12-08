b91e1de3e232c3419bfdcaa9ab689cde9e3ed14a
h-rub <rubio@loopgk.com>
2021-12-07 20:44:07 -0600

    Polls all route

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from polls.models import Polls, Vote
+from polls.models import Options, Polls, Vote
 
 # Register your models here.
 admin.site.register(Polls)
+admin.site.register(Options)
 admin.site.register(Vote)
