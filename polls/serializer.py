bc23319f0fcc0d6f5681e32c10cfac7cd4818a8a
h-rub <rubio@loopgk.com>
2021-12-07 21:58:12 -0600

    New Polls Serializer

--- polls/serializer.py
+++ polls/serializer.py
@@ -7,13 +7,19 @@ from datetime import datetime
 class PollsSerializer(serializers.ModelSerializer):
 
     created_by = serializers.SerializerMethodField('get_full_name')
+    options = serializers.SerializerMethodField('get_options')
 
     class Meta:
         model = Polls
-        fields = ['pk', 'title', 'isClosed', 'created', 'modified', 'created_by']
+        fields = ['pk', 'title', 'isClosed', 'created', 'modified', 'created_by', 'options']
 
     def get_full_name(self, Polls):
         _id = Polls.created_by.user.pk
         user = CustomUser.objects.get(id=_id)
         full_name = str(user)
         return full_name
+
+    def get_options(self, Polls):
+        _id = Polls.pk
+        data = {"id_pk": _id}
+        return data
