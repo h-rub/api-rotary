303cfab478ac9853e59e8589f2862ceb44236eb2
h-rub <rubio@loopgk.com>
2021-12-07 22:04:37 -0600

    New Polls Serializer

--- polls/serializer.py
+++ polls/serializer.py
@@ -21,7 +21,7 @@ class PollsSerializer(serializers.ModelSerializer):
 
     def get_options(self, Polls):
         _id = Polls.pk
-        poll = Polls.objects.get(pk = _id)
+        poll = Polls.__class__.objects.get(pk = _id)
         option = Polls.__class__.objects.all().filter(poll = poll)
         data = {"id_pk": _id, "options": option}
         return data
